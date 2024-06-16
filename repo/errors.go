61759b14a9fb058ae4bb0b0855005ad83b62eb2d
marlonmp <marlonmp214@protonmail.com>
2024-06-16 18:58:20 -0500

    conflict error added

--- repo/errors.go
+++ repo/errors.go
@@ -1,10 +1,15 @@
 package repo
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/jackc/pgx"
+)
 
 const (
 	NotFoundErrorCode     = "resource_not_found"
 	DoesNotExistErrorCode = "resource_does_not_exist"
+	ConflictErrorCode     = "resource_already_exist"
 )
 
 // NotFoundError must be returned when a resource may exist but after
@@ -14,7 +19,7 @@ type NotFoundError struct {
 }
 
 func (enf NotFoundError) Error() string {
-	return "not found err: Resource not found"
+	return "not found err: resource not found"
 }
 
 func (enf NotFoundError) Unwrap() error {
@@ -34,6 +39,21 @@ func IsNotFoundError(err error) bool {
 	return errors.As(err, &enf)
 }
 
+// if the err is compatible, it gets wrapped into a NotFoundError
+// and returns true, else do nothig and returns false
+func AsNotFoundError(err *error) bool {
+	if err == nil || *err == nil {
+		return false
+	}
+
+	if errors.Is(*err, pgx.ErrNoRows) {
+		*err = NotFoundError{*err}
+		return true
+	}
+
+	return false
+}
+
 // DoesNotExistError must be returned when a resource such as a file or endpoint
 // does not exist or has not been created. In can also be used when an user
 // does not have permission to access a file or endpoint.
@@ -42,7 +62,7 @@ type DoesNotExistError struct {
 }
 
 func (dne DoesNotExistError) Error() string {
-	return "does not exist err: Resource does not exist"
+	return "does not exist err: resource does not exist"
 }
 
 func (dne DoesNotExistError) Unwrap() error {
@@ -60,4 +80,29 @@ func (dne DoesNotExistError) Message() string {
 func IsDoesNotExistError(err error) bool {
 	var dne *DoesNotExistError
 	return errors.As(err, &dne)
+}
+
+type ConflictError struct {
+	err error
+}
+
+func (ce ConflictError) Error() string {
+	return "conflict err: resource already exist"
+}
+
+func (ce ConflictError) Unwrap() error {
+	return ce.err
+}
+
+func (ce ConflictError) Code() string {
+	return ConflictErrorCode
+}
+
+func (ce ConflictError) Message() string {
+	return ""
+}
+
+func IsConflictError(err error) bool {
+	var ce *ConflictError
+	return errors.As(err, &ce)
 }
